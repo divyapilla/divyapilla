c3510ddc075aaa44e5dd2852660d0267882bd059
unknown <divyadolly506@gmail.com>
2021-10-20 05:23:38 -0700

    add support for connStr via env variable'

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
+  //const db = config.get('db');
+  mongoose.connect('mongodb+srv://divya:divya506@cluster0.1z97m.mongodb.net/test')
     .then(() => winston.info(`Connected to ${db}...`));
 }
